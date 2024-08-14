d445a92ba9ee0b7661e0299569474ef0d7d82512
Gustavo Pavão <gustavopavaoo@icloud.com>
2024-08-14 04:11:30 -0300

    project finished

--- projeto-conta-bancaria/conta-banco/src/ContaTerminal.java
+++ projeto-conta-bancaria/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        String name;
+        String agencia;
+        int numero;
+        BigDecimal saldo;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        name = entrada.nextLine();
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = entrada.nextLine();
+        System.out.println("Por favor, digite número da Conta: ");
+        numero = entrada.nextInt();
+        System.out.println("Por favor, digite o seu saldo: ");
+        saldo = entrada.nextBigDecimal();
+        entrada.close();
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco. A sua agência é " 
+        + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
